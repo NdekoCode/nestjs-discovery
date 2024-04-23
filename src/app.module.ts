efd56e6fa838e8fd0ee7837bdeaa7bd962759b32
Ndekocode <arickbulakali@gmail.com>
2024-04-23 12:11:30 +0200

    create my first modules: recipe and ingredient

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IngredientModule } from './ingredient/ingredient.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [],
+  imports: [RecipeModule, IngredientModule],
   controllers: [AppController],
   providers: [AppService],
 })
