2485912df06cda222c1b17826af0d9e9181b19aa
Ndekocode <arickbulakali@gmail.com>
2024-04-23 11:30:59 +0200

    refactor: generate new project using nest-cli

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  sayHello() {
-    return "Hello nest.js";
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  app.listen(PORT);
+  await app.listen(3000);
 }
 bootstrap();
