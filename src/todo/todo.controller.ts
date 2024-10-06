e736fe4fbecd46656018b343a8c42b83eca3ba67
Arick Bulakali <arickbulakali@gmail.com>
2024-10-06 07:42:50 +0200

    refactor: add validation pipe

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,9 @@
 import { Request } from 'express';
 import { Todo } from 'libs/types';
 
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req } from '@nestjs/common';
+import {
+    Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Post, Put, Query, Req
+} from '@nestjs/common';
 
 import { AddTodoDTO } from './dto/add-todo.dto';
 import { GetPaginatedTodoDTO } from './dto/get-paginated-todo.dto';
@@ -11,31 +13,49 @@ import { TodoService } from './todo.service';
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
   @Get()
-  getTodos(@Req() req: Request,@Query() query:GetPaginatedTodoDTO): Todo[] {
-    console.log(req);
+  getTodos(@Req() req: Request, @Query() query: GetPaginatedTodoDTO): Todo[] {
     let todos = this.todoService.todos;
-    if(query && query.page){
-      todos = todos.slice(query.page,query.limit).filter((_,index)=>index<=query.limit);
+    console.log('Query', query);
+    if (query) {
+      console.log(
+        'TODO QUERY.PAGE',
+        typeof query.page,
+        typeof query.limit,
+        todos.slice(query.page),
+      );
+
+      todos = todos.slice(query.page).filter((_, index) => index < query.limit);
     }
-     return todos;
+    return todos;
   }
 
   @Get(':id')
-  singleTodo(@Param('id') id: string): Todo {
+  singleTodo(@Param('id', ParseIntPipe) id: string): Todo {
     return this.todoService.getSingleTodo(id);
   }
   @Post()
-  addTodo(@Body() todo:AddTodoDTO): Todo {
+  addTodo(@Body() todo: AddTodoDTO): Todo {
     return this.todoService.addTodo(todo);
   }
 
   @Put(':id')
-  updateTodo(@Param('id') id: string, @Body() todo: Partial<Todo>): Todo {
+  updateTodo(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() todo: Partial<Todo>,
+  ): Todo {
     return this.todoService.updateTodo(id, todo);
   }
-  
+
   @Delete(':id')
-  deleteTodo(@Param('id') id: string): { message: string } {
+  deleteTodo(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_FOUND,
+      }),
+    )
+    id: string,
+  ): { message: string } {
     return this.todoService.deleteTodo(id);
   }
 }
